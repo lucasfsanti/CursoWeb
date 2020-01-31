39dc58d5415ac650481150f48bbe9dc6ed845042
lucasfsanti <47954905+lucasfsanti@users.noreply.github.com>
2020-01-31 01:08:58 -0300

    Aulas de funções em JS

---
+++ Fundamentos/funcoes.js
@@ -0,0 +1,38 @@
+// Funcao sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b);
+}
+
+imprimirSoma(2, 3);
+
+imprimirSoma(2);
+imprimirSoma(2, 3, 4, 5, 6, 7, 8, 9, 10);
+imprimirSoma();
+
+// Funcao com retorno
+function soma(a, b = 1) {
+    return a + b;
+}
+
+console.log(soma(2, 3));
+
+console.log(soma(2));
+console.log(soma());
+
+// Armazenar função em variável
+const imprimirSoma2 = function (a, b) {
+    console.log(a + b);
+}
+
+imprimirSoma2(2,3);
+
+// Armazenar função arrow em uma variável
+const soma2 = (a, b) => {
+    return a + b;
+}
+
+console.log(soma2(2, 3));
+
+// Retorno implicito
+const subtracao = (a, b) => a - b;
+console.log(subtracao(2, 3));
